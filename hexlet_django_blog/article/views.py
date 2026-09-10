f76aed77d572becd3417b14dde52818fbad14918
Qualcommes <qualcommes122@gmail.com>
2026-09-10 21:36:28 +0700

    sixth step

--- hexlet_django_blog/article/views.py
+++ hexlet_django_blog/article/views.py
@@ -1,5 +1,9 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("article")
+    return render(
+        request,
+        "articles/main_view.html",
+        {"app_name": "hexlet-django-blog"}
+        )
